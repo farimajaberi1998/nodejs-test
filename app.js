a04e2935c484e52a915cc4568959e36d239b2816
farima.jaberi <farimajaberii@gmail.com>
2024-09-17 11:28:23 +0330

    put method

--- app.js
+++ app.js
@@ -14,11 +14,12 @@ const courses = [
   },
 ];
 app.get("api/courses/:id", (req, res) => {
-  const course = course.find((c) => c.id === Number(req.params.id));
+  const course = course.find((c) => c.id === parseInt(req.params.id));
   res.send(course);
 });
 
-app.post("api/courses", (req, res) => {
+//POST METHOD//
+app.post("api/courses/:id", (req, res) => {
   if (!req.body.name || req.body.name.length < 3) {
     res.status(400).send("name is required");
     return;
@@ -30,6 +31,20 @@ app.post("api/courses", (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+
+//PUT METHOD//
+app.put("api/courses/:id",(req, res)=>{
+  const course = courses.find(c=> c.id === parseInt(req.params.id))
+  if(!course) return 
+  res.status(404).send("course with given id not found")
+
+  if(!req.body.name || req.body.length > 3)
+    return res.status(400).send("name is require and more than 3 character")
+  course.name = req.body.name
+  res.send(course)
+})
+
 
 const Port = process.env.APP_PORT || 3000;
 app.listen(Port, () => {
